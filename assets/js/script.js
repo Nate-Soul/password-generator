b03c49d97449d2b2cd02e2d40aa79b59e8a17c87
Nate-Soul <nanithsukpong@gmail.com>
2023-05-22 22:24:58 +0100

    password generator function with UI

---
+++ assets/js/script.js
@@ -0,0 +1,55 @@
+//generate password script
+
+//[Edit this]
+const copyBtn   = document.querySelector("#copyBtn");
+const pasteBtn  = document.querySelector("#pasteBtn");
+const genBtn    = document.querySelector("#genPsw");
+const pswField  = document.querySelector("#password");
+const cPswField = document.querySelector("#cPassword");
+
+genBtn.addEventListener("click", () =>  {
+    const chars = "0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    const pswLength = parseInt(document.querySelector("#passwordLength").value);
+    let genPsw = "";
+
+    if(pswLength < 3 || pswLength > 64){
+        alert("Password length is too short or too long: Minimu of 3 and maximum of 64");
+        return;
+    }
+
+    const array = new Uint32Array(chars.length);
+    window.crypto.getRandomValues(array);
+
+    for(let i = 0; i < pswLength; i++){
+        genPsw += chars[array[i] % chars.length];
+    }
+
+    pswField.value = genPsw;
+});
+
+
+copyBtn.addEventListener("click", () => {
+    const copyText = pswField.value;
+        navigator.clipboard.writeText(copyText).then(
+            //[edit this]
+            success => alert("text copied into clipboard"),
+            err => alert(err)
+        );
+});
+
+// paste password when the confirm password input is focused
+cPswField.addEventListener("focus", () => {
+    navigator.clipboard.readText().then(
+        cliptext => cPswField.value = cliptext, 
+        cPswField.blur(),
+        err => console.log(err)
+    );
+});
+
+//alternative - paste password when paste password button is clicked
+pasteBtn.addEventListener("click", () => {
+    navigator.clipboard.readText().then(
+        cliptext => cPswField.value = cliptext,
+        err => console.log(err)
+    );
+});
